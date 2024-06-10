fa735c37719b92963f2f423ef8956c24dd87601b
Aaron <aakiswa@gmail.com>
2024-06-10 18:10:21 +0800

    Add draw check function

--- script.js
+++ script.js
@@ -26,6 +26,42 @@ const Gameboard = (() => {
       };
 
       const checkPattern = () => {
+            const checkLine = (a, b, c) => {
+                  return a === b && b === c && a !== undefined;
+            }
+
+            // Row/Column Checker
+            for(let cell = 0; cell < 3; cell++) {
+                  if(checkLine(gameBoard[cell][0], gameBoard[cell][1], gameBoard[cell][2])){
+                        checkWinner(gameBoard[cell][0]);
+                  }else if(checkLine(gameBoard[0][cell], gameBoard[1][cell], gameBoard[2][cell])){
+                        checkWinner(gameBoard[0][cell]);
+                  };
+            }
+
+            // Diagonal Checker
+            if(checkLine(gameBoard[0][0], gameBoard[1][1], gameBoard[2][2])){
+                  checkWinner(gameBoard[1][1]);
+            }else if(checkLine(gameBoard[0][2], gameBoard[1][1], gameBoard[2][0])){
+                  checkWinner(gameBoard[1][1]);
+            }
+
+            const drawCheck = () => {
+                  let turnCount = 0;
+                  for(let i = 0; i < 3; i++) {
+                        for(let j = 0; j < 3; j++) {
+                              if(gameBoard[i][j] !== undefined) {
+                                    turnCount++;
+                              }
+                        }
+                  }
+                  if(turnCount === 9) {
+                        console.log("It's a draw");
+                        restartGameBoard();
+                  }      
+            }
+            drawCheck();
+
             const checkWinner = (token) => {
                   if(token === "X") {
                         console.log("X Win");
@@ -35,25 +71,6 @@ const Gameboard = (() => {
                   restartGameBoard();
             }
 
-            const checkLine = (a, b, c) => {
-                  return a === b && b === c && a !== undefined;
-            }
-
-            //Row/Column Checker
-            for(let cell = 0; cell < 3; cell++) {
-                  if(checkLine(gameBoard[cell][0], gameBoard[cell][1], gameBoard[cell][2])){
-                        checkWinner(gameBoard[cell][0]);
-                  }else if(checkLine(gameBoard[0][cell], gameBoard[1][cell], gameBoard[2][cell])){
-                        checkWinner(gameBoard[0][cell]);
-                  };
-            }
-
-            //Diagonal Checker
-            if(checkLine(gameBoard[0][0], gameBoard[1][1], gameBoard[2][2])){
-                  checkWinner(gameBoard[1][1]);
-            }else if(checkLine(gameBoard[0][2], gameBoard[1][1], gameBoard[2][0])){
-                  checkWinner(gameBoard[1][1]);
-            }
       };
 
       return {updateGameBoard, restartGameBoard, gameBoard};
@@ -74,26 +91,18 @@ const gameFlow = {
 }
 
 // Testing
-// Gameboard.updateGameBoard(0, 0, Player1["token"]);
-// Gameboard.updateGameBoard(0, 1, Player2["token"]);
-Gameboard.updateGameBoard(0, 2, Player1["token"]);
-// Gameboard.updateGameBoard(1, 0, Player1["token"]);
+Gameboard.updateGameBoard(0, 0, Player2["token"]);
+Gameboard.updateGameBoard(0, 1, Player1["token"]);
+Gameboard.updateGameBoard(0, 2, Player2["token"]);
+Gameboard.updateGameBoard(1, 0, Player2["token"]);
 Gameboard.updateGameBoard(1, 1, Player1["token"]);
-// Gameboard.updateGameBoard(1, 2, Player2["token"]);
+Gameboard.updateGameBoard(1, 2, Player2["token"]);
 Gameboard.updateGameBoard(2, 0, Player1["token"]);
-// Gameboard.updateGameBoard(2, 1, Player2["token"]);
+Gameboard.updateGameBoard(2, 1, Player2["token"]);
 Gameboard.updateGameBoard(2, 2, Player1["token"]);
-
-
 
 console.table(Gameboard.gameBoard);
 
-
-// Gameboard.restartGameBoard();
-
-// Gameboard.updateGameBoard(0, 1, Player2["token"]);
-
-// console.table(Gameboard.gameBoard);
 
 
 
