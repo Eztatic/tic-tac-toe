cbabcc62986fa729f9047f9c1ddd342cca2510ab
Aaron <aakiswa@gmail.com>
2024-06-13 00:39:17 +0800

    Optimize code

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const Gameboard = (() => {
+const GameBoard = () => {
       let gameBoard = [];
-      let cellValue;
+
+      const Cell = () => {
+            let emptyValue = true;
+            let value = "";
+
+            const isEmpty = () => { return emptyValue; }
+            const getValue = () => { return value; }
+            const setValue = (newValue) => { 
+                  value = newValue
+                  if(value !== "") {
+                        emptyValue = false;
+                  }
+            }
+
+            return {isEmpty, getValue, setValue};
+      }
 
       const generateGameBoard = () => {
             for(let i = 0; i < 3; i++){
                   gameBoard[i] = [];
                   for(let j = 0; j < 3; j++){
-                        gameBoard[i].push(undefined);
+                        gameBoard[i].push(Cell());
                   }
             }
       }
@@ -20,19 +35,17 @@ const Gameboard = (() => {
             generateGameBoard();
       };
 
-      const updateGameBoard = (a, b, c) => {
-            if(gameBoard[a][b] === undefined) {
-                  gameBoard[a][b] = c;
+      const updateGameBoard = (row, col, token) => {
+            if(gameBoard[row][col].isEmpty() === true) {
+                  gameBoard[row][col].setValue(token);
                   checkPattern();
             }
       };
 
       const checkWinner = (token) => {
-            if(token === "X") {
-                  Player1.score++;
+            if(token.getValue() === "X") {
                   console.log("X Win");
-            }else if(token === "O") {
-                  Player2.score++;
+            }else if(token.getValue() === "O") {
                   console.log("O Win");  
             }
             Player1.turn = false;
@@ -41,18 +54,17 @@ const Gameboard = (() => {
       }
 
       const checkPattern = () => {
-            const checkLine = (a, b, c) => {
-                  return a === b && b === c && a !== undefined;
-            }
+            const checkLine = (row, col, token) => {
+                  const [rowVal, colVal, tokenVal] = [row.getValue(), col.getValue(), token.getValue()];
+                  return rowVal === colVal && colVal === tokenVal && rowVal !== "";
+            };
 
             // Row/Column Checker
             for(let cell = 0; cell < 3; cell++) {
                   if(checkLine(gameBoard[cell][0], gameBoard[cell][1], gameBoard[cell][2])){
                         checkWinner(gameBoard[cell][0]);
-                        restartGameBoard();
                   }else if(checkLine(gameBoard[0][cell], gameBoard[1][cell], gameBoard[2][cell])){
-                        checkWinner(gameBoard[0][cell]);
-                        
+                        checkWinner(gameBoard[0][cell]);  
                   };
             }
 
@@ -64,15 +76,15 @@ const Gameboard = (() => {
             }
 
             const drawCheck = () => {
-                  let turnCount = 0;
-                  for(let i = 0; i < 3; i++) {
-                        for(let j = 0; j < 3; j++) {
-                              if(gameBoard[i][j] !== undefined) {
-                                    turnCount++;
+                  let occupiedCell = 0;
+                  gameBoard.forEach((row) => {
+                        row.forEach((cell) => {
+                              if(cell.isEmpty() !== true) {
+                                    occupiedCell++;
                               }
-                        }
-                  }
-                  if(turnCount === 9) {
+                        })
+                  });
+                  if(occupiedCell === 9) {
                         console.log("It's a draw");
                         restartGameBoard();
                   }      
@@ -81,55 +93,48 @@ const Gameboard = (() => {
       };
 
       return {generateGameBoard, updateGameBoard, restartGameBoard, getGameboard, checkWinner};
-})();
+};
 
-const Player1 = {
-      token : "X",
-      score : 0,
-      turn: true
-}
+const GameController = () => {
 
-const Player2 = {
-      token : "O",
-      score : 0,
-      turn: false
-}
+      const theGameBoard = GameBoard(); 
 
-const player1Turn = (row, col) => {
-      Player1.turn = false;
-      Player2.turn = true;
-      Gameboard.updateGameBoard(row, col, Player1.token);
-}
+      const Player1 = {
+            token : "X",
+            score : 0,
+            turn: false
+      }
+      
+      const Player2 = {
+            token : "O",
+            score : 0,
+            turn: false
+      }
 
-const player2Turn = (row, col) => {
-      Player1.turn = true;
-      Player2.turn = false;
-      Gameboard.updateGameBoard(row, col, Player2.token);
-}
+      let activePlayer = Player1;
 
-// const startGame = document.querySelector("button");
-// startGame.addEventListener("click", (e) => {
-//       e.preventDefault();
-//       Player1.turn = true;
-//       for(let p = 0; p < 9; p++){
-//             if(Player1.turn === false && Player2.turn === false) {
-//                   break;
-//             }
-//             let playerInput = prompt("Player 1/2:");
-//             if(!playerInput) {break;}
-//             const row = Number(playerInput[0]);
-//             const col = Number(playerInput[1]);
-//             // const [row, col] = playerInput.split(',').map(Number);
-//             if(Player1.turn) {
-//                   player1Turn(row, col);
-//             }else {
-//                   player2Turn(row, col);
-//             }
-//             console.table(Gameboard.getGameboard());
-//       }
-// });
+      const switchPlayer = (currentPlayer) => {
+            if(activePlayer === currentPlayer){
+                  currentPlayer.turn = true;
+            }
+      }
 
-const startRound = () => {
+      const playRound = (row) => {
+            GameBoard.updateGameBoard(a, b, c)
+      }
+      
+      const player1Turn = (row, col) => {
+            Player1.turn = false;
+            Player2.turn = true;
+            Gameboard.updateGameBoard(row, col, Player1.token);
+      }
+      
+      const player2Turn = (row, col) => {
+            Player1.turn = true;
+            Player2.turn = false;
+            Gameboard.updateGameBoard(row, col, Player2.token);
+      }
+
       Player1.turn = true;
       for(let p = 0; p < 9; p++){
             if(Player1.turn === false && Player2.turn === false) {
@@ -160,6 +165,28 @@ const startRound = () => {
 // Gameboard.updateGameBoard(2, 2, Player1["token"]);
 
 // console.table(Gameboard.getGameboard());
+
+// const startGame = document.querySelector("button");
+// startGame.addEventListener("click", (e) => {
+//       e.preventDefault();
+//       Player1.turn = true;
+//       for(let p = 0; p < 9; p++){
+//             if(Player1.turn === false && Player2.turn === false) {
+//                   break;
+//             }
+//             let playerInput = prompt("Player 1/2:");
+//             if(!playerInput) {break;}
+//             const row = Number(playerInput[0]);
+//             const col = Number(playerInput[1]);
+//             // const [row, col] = playerInput.split(',').map(Number);
+//             if(Player1.turn) {
+//                   player1Turn(row, col);
+//             }else {
+//                   player2Turn(row, col);
+//             }
+//             console.table(Gameboard.getGameboard());
+//       }
+// });
 
 
 
